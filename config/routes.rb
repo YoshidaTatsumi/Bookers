6972f1445c0f4ce7fe2de6e5ea51857b42c833ad
そそ <sora@sosonoMacBook-Air.local>
2020-01-06 17:57:07 +0900

    [Add]CREATE

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'books#top'
-  resources :books
+  post 'books' => 'books#create'
+  resources :books, only:[:index, :show, :edit, :update, :destroy]
 end
